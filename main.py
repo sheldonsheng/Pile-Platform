8130ae1defb6106702ac96bcfa8679ea2c1e3e57
sheldonsheng <shengxuyuan@126.com>
2022-01-28 10:23:49 +0800

    def Dataframe

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # This is a simple platform for consultanting foundation design projects
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+
+class Bored_Pile:
+    def __init__(self, dia, l):
+        #初始化灌注桩属性(直径、桩长)
+        self.dia = dia
+        self.l = l
+
+df_grout_phi = pd.DataFrame({''})
+
+#表1 - 后注浆桩侧增强系数、后注浆桩端增强系数、
+df_size_phi = pd.DataFrame({'clay/silt': [(0.8/Bored_Pile.dia)^(1/5), (0.8/Bored_Pile.dia)^(1/4)]}, \n
+)
+#表2 - 尺寸效应系数桩侧、尺寸效应系数桩端
